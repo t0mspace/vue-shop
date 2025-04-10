fa1e08035f13dd7531e83ef639a225601985bc96
Thomas Brenard <thomasx2005@gmail.com>
2025-04-10 14:02:40 +0200

    add price range

--- src/components/Shop/ShopFilters.vue
+++ src/components/Shop/ShopFilters.vue
@@ -24,49 +24,26 @@ const emit = defineEmits<{
     </section>
     <section>
       Choisir la fourchette des prix
-      <div class="mb-5">
-        <label for="0-10000">De 0 à 10000 eur</label>
+      <div
+        class="mb-5"
+        v-for="(priceRange, index) in [
+          [0, 10000],
+          [800, 1000],
+          [1000, 1500],
+          [1500, 2000],
+        ] as [number, number][]"
+        :key="index"
+      >
+        <label :for="`${priceRange[0]}-${priceRange[1]}`">
+          {{ priceRange[0] }} - {{ priceRange[1] }}
+        </label>
         <input
+          :checked="filters.priceRange[0] === priceRange[0]"
           type="radio"
-          @input="emit('updateFilter', { priceRange: [0, 10000] })"
+          @input="$emit('updateFilter', { priceRange })"
           name="priceRange"
-          id="0-10000"
-        />
-      </div>
-      <div class="mb-5">
-        <label for="800-1000">800-1000</label>
-        <input
-          type="radio"
-          name="priceRange"
-          id="800-1000"
-          @input="emit('updateFilter', { priceRange: [800, 1000] })"
-        />
-      </div>
-      <div class="mb-5">
-        <label for="1000-1500">1000-1500</label>
-        <input
-          type="radio"
-          name="priceRange"
-          id="1000-1500"
-          @input="emit('updateFilter', { priceRange: [1000, 1500] })"
-        />
-      </div>
-      <div class="mb-5">
-        <label for="1500-2000">1500-2000</label>
-        <input
-          @input="emit('updateFilter', { priceRange: [1500, 2000] })"
-          type="radio"
-          name="priceRange"
-          id="1500-2000"
-        />
-      </div>
-      <div class="mb-5">
-        <label for="2000-10000">2000-10000</label>
-        <input
-          @input="emit('updateFilter', { priceRange: [2000, 10000] })"
-          type="radio"
-          name="priceRange"
-          id="2000-10000"
+          :id="`${priceRange[0]}-${priceRange[1]}`"
+          :value="`${priceRange[0]}-${priceRange[1]}`"
         />
       </div>
     </section>
